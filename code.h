dd274f20e94d0e56ea5de97caf1b82d58cd39f74
Naplesoul <shenwhang@outlook.com>
2021-03-05 21:14:53 +0800

    reorganize code structure

---
+++ code.h
@@ -0,0 +1,16 @@
+#ifndef CODE_H
+#define CODE_H
+
+#include <QObject>
+
+class Code : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Code(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // CODE_H
